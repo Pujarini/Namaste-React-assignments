a8c1fa63d8fe1790631fa88d9ba51a0f00caf917
Pujarini Jena <pujarinijena@Pujarini-MacBook-Pro.local>
2022-11-24 14:50:08 +0530

    Day-3 assignment

---
+++ Day-3/src/App.js
@@ -0,0 +1,38 @@
+import React, { createElement } from "react";
+import ReactDOM from "react-dom";
+import Header from "./components/Header";
+
+// without JSX
+const heading = createElement("div", { id: "title" }, [
+  createElement("h1", { id: "heading1" }, "Hello World"),
+  createElement("h2", { id: "heading2" }, "Hello World1"),
+  createElement("h3", { id: "heading3" }, "Hello World2"),
+]);
+
+//using JSX
+const heading2 = (
+  <div className="title">
+    <h1 id="heading1">Hello World</h1>
+    <h1 id="heading2">Hello World1 jsx</h1>
+    <h1 id="heading3">Hello World2 jsx</h1>
+  </div>
+);
+
+const MainApp = () => {
+  return (
+    <>
+      <Header />
+      {/* <div>Main App</div> */}
+      {/* <ShowHeading /> */}
+    </>
+  );
+};
+
+const ShowHeading = () => {
+  return heading2;
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading2);
+//root.render(<ShowHeading/>)
+root.render(<MainApp />);
